1d16b01861708b2b593fc148c101a477d54f1f26
Greg Davidson <gddavidson99@gmail.com>
2017-10-21 17:58:19 -0400

    prototype

--- js/main.js
+++ js/main.js
@@ -4,60 +4,54 @@ var success = 0;
 // CHANGE THE SIZE OF THE BUTTON ON HOVER
 $(function() {
 
-	loadin();
+    loadin();
 
-	$('#addItem').mouseenter(function()
-	{
-		$('#addItem').animate({height: '50px', width: '50px', lineHeight: '45px'}, "fast");
-	});
+    $('#addItem').mouseenter(function()
+    {
+        $('#addItem').animate({height: '50px', width: '50px', lineHeight: '45px'}, "fast");
+    });
 
-	$('#addItem').mouseleave(function()
-	{
-		$('#addItem').animate({height: '45px', width: '45px', lineHeight: '40px'}, "fast");
-	});
+    $('#addItem').mouseleave(function()
+    {
+        $('#addItem').animate({height: '45px', width: '45px', lineHeight: '40px'}, "fast");
+    });
 
 });
 
 // New Item is clicked
 $('#newItem').click(function() {
 
-	var items1 = new Array();
-	items1 = JSON.parse(localStorage.getItem('entries'));
+    var items1 = new Array();
 
-	// Transer values to variables
-	var Description = $('#description').val();
-	var Day = $('#date').val();
-	var Time = $('#time').val();
+		if(localStorage.getItem('entries') != '')
+		{items1 = JSON.parse(localStorage.getItem('entries'));}
 
-	entries[entries.length]={desc:Description, monthday:Day, clock:Time};
+    // Transer values to variables
+    var Description = $('#description').val();
+    var Day = $('#date').val();
+    var Time = $('#time').val();
 
-	var entry = new Object();
+    var entry = new Object();
 
-	entry.desc = Description;
-	entry.monthday = Day;
-	entry.clock = Time;
+    entry.desc = Description;
+    entry.monthday = Day;
+    entry.clock = Time;
 
-	items1.push(entry);
+    items1.push(entry);
 
-	var entry = new Object();
-	entry.desc = Description;
-	entry.monthday = Day;
-	entry.clock = Time;
-
-	for(var i = 0; i<items1.length;i++)
-	{
-		console.log(items1[i]);
-	}
-
-	// Add values to table
+    // Add values to table
     $('#itemsTable tbody').prepend("<tr class='animated fadeInLeft'><td>" + Description + "</td><td>"+ Day + "</td><td>" + Time + "</td><td>" + "<button type='button' class='btn btn-success pull-right' id='clear'>Clear</button><button type='button' class='btn btn-success pull-right' id='complete'>Complete</button>" + "</td><tr>");
 
-	// Reset form
-	$('#form')[0].reset();
-	localStorage.setItem('entries', JSON.stringify(items1));
-	localStorage.setItem('itemsTable', itemsTable);
+    // Reset form
+    $('#form')[0].reset();
+    localStorage.setItem('entries', JSON.stringify(items1));
+    localStorage.setItem('itemsTable', itemsTable);
 
 });
+
+$('#clear').click(function() {
+
+})
 
 function remove(array, element) {
     const index = array.indexOf(element);
@@ -69,99 +63,87 @@ function remove(array, element) {
 
 function loadin()
 {
-	var curday = getDate();
-	var curmonth = getMonth();
-	var hours = getHours();
-	var minutes = getMinutes();
-	entries = localStorage.getItem('entries');
 
-function loadin()
-{
+    var entries = new Array();
 
-	var entries = new Array();
+    //Get current time
+    var curday = new Date().getDate();
+    var curmonth = new Date().getMonth();
+    var hours = new Date().getHours();
+    var minutes = new Date().getMinutes();
 
-	//Get current time
-	var curday = new Date().getDate();
-	var curmonth = new Date().getMonth();
-	var hours = new Date().getHours();
-	var minutes = new Date().getMinutes();
+    //get stored variable
+		if(localStorage.getItem('entries') != '')
+		{entries = JSON.parse(localStorage.getItem('entries'));
+    localStorage.setItem('entries', JSON.stringify(entries));}
+    // entries = JSON.parse(localStorage.getItem('entries'));
+    // localStorage.setItem('entries', JSON.stringify(entries));
 
-	//get stored variable
+    /*
 
-	entries = JSON.parse(localStorage.getItem('entries'));
-	localStorage.setItem('entries', JSON.stringify(entries));
+    if(localStorage.)
+    entries = localStorage.getItem('entries');
+    console.log(entries);
 
-	for (var i = 0; i < entries.length; i++) {
+    //loop through each entry
+    for(var i= 0; i<entries.length;i++)
+    {
+        //get the string of the entered month until the slash and compare that to the current month. If it is lower the entry is removed
+        if(parseInt(entries(i).monthday.slice(0,indexOf("/"))<curmonth))
+        {
+            remove(entries,i);
+            fails++;
 
-		console.log(entries[i]);
+        }
 
-	}
+        //if the month is equal the day needs to be checked
 
-	/*
+            //get the day by substringing the entered date after the /. compare to current day and if the entered is lower delete the entry
 
-	if(localStorage.)
-	entries = localStorage.getItem('entries');
-	console.log(entries);
+            if(parseInt(entries(i).monthday.slice(indexOf("/")+1)<curday))
+            {
+                remove(entries,i);
+                fails++;
 
-	//loop through each entry
-	for(var i= 0; i<entries.length;i++)
-	{
-		//get the string of the entered month until the slash and compare that to the current month. If it is lower the entry is removed
-		if(parseInt(entries(i).monthday.slice(0,indexOf("/"))<curmonth))
-		{
-			remove(entries,i);
-			fails++;
+            }
 
-		}
+            //if the days are equal time needs to be checked
 
-		//if the month is equal the day needs to be checked
+                //hours is gotten by substringing the time until the :. Then compared to current time and if lower removed. if equal do for minutes
 
-			//get the day by substringing the entered date after the /. compare to current day and if the entered is lower delete the entry
+                if(parseInt(entries(i).clock.slice(0,indexOf(":"))<hours))
+                {
+                        remove(entries,i);
+                        fails++;
 
-			if(parseInt(entries(i).monthday.slice(indexOf("/")+1)<curday))
-			{
-				remove(entries,i);
-				fails++;
-
-			}
-
-			//if the days are equal time needs to be checked
-
-				//hours is gotten by substringing the time until the :. Then compared to current time and if lower removed. if equal do for minutes
-
-				if(parseInt(entries(i).clock.slice(0,indexOf(":"))<hours))
-				{
-						remove(entries,i);
-						fails++;
-
-				}
-				else if (parseInt(entries(i).clock.slice(0,indexOf(":"))==hours)) {
+                }
+                else if (parseInt(entries(i).clock.slice(0,indexOf(":"))==hours)) {
 
 
-					if (parseInt(entries(i).clock.slice(indexOf(":")+1)<=minutes)) {
+                    if (parseInt(entries(i).clock.slice(indexOf(":")+1)<=minutes)) {
 
 
 
 
-					if (parseInt(entries(i).clock.slice(indexOf(":")+1,indexOf(":")+3)<=minutes)) {
+                    if (parseInt(entries(i).clock.slice(indexOf(":")+1,indexOf(":")+3)<=minutes)) {
 
 
-							remove(entries,i);
-							fails++;
+                            remove(entries,i);
+                            fails++;
 
-					}
-				}
-			}
+                    }
+                }
+            }
 
-		}
-	}
-	*/
+        }
+    }
+    */
 
-	//reprint the table
-	for(var i= 0; i<entries.length;i++)
-	{
-		$('#itemsTable tbody').prepend("<tr class='animated fadeInLeft'><td>" + entries[i].desc + "</td><td>"+ entries[i].monthday + "</td><td>" + entries[i].clock + "</td><td>" + "<button type='button' class='btn btn-success pull-right' id='clear'>Clear</button><button type='button' class='btn btn-success pull-right' id='complete'>Complete</button>" + "</td><tr>");
-	}
+    //reprint the table
+    for(var i= 0; i<entries.length;i++)
+    {
+        $('#itemsTable tbody').prepend("<tr class='animated fadeInLeft'><td>" + entries[i].desc + "</td><td>" + entries[i].monthday + "</td><td>" + entries[i].clock + "</td><td>" + "<button type='button' class='btn btn-success pull-right' id='clear'>Clear</button><button type='button' class='btn btn-success pull-right' id='complete'>Complete</button>" + "</td></tr>");
+    }
 
 }
 
@@ -202,7 +184,7 @@ function loadin()
     Progressbar.prototype.update = function (value) {
         var $div = this.$element.find('div');
         var $span = $div.find('span');
-				var value = success/(success+fails);
+                var value = success/(success+fails);
         $div.attr('aria-valuenow', value);
         $div.css('width', value + '%');
         $span.text(value + '% Complete');
