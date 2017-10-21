b629832bb9c03fe584463ebfe483e479ec18f588
Greg Davidson <gddavidson99@gmail.com>
2017-10-21 11:41:34 -0400

    Biggggg Merge

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 // progressbar.js@1.0.0 version is used
 // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 
+$(function() {
+
+	$('#addItem').mouseenter(function() {
+
+		$('#addItem').animate({height: '50px', width: '50px', lineHeight: '45px'}, "fast");
+
+	});
+
+	$('#addItem').mouseleave(function() {
+
+		$('#addItem').animate({height: '45px', width: '45px', lineHeight: '40px'}, "fast");
+
+	});
+
+});
+
+/*
 var bar = new ProgressBar.SemiCircle(container, {
 
   var score = 0;
@@ -39,3 +56,4 @@ bar.text.style.fontSize = '2rem';
 
 bar.animate(percent);  // Number from 0.0 to 1.0
 }
+*/
