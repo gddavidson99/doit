ef573679f13b228e60bcdd5bf988c5c51ed13493
jlew248 <32982171+jlew248@users.noreply.github.com>
2017-10-21 14:12:57 -0400

    kinda works

--- js/main.js
+++ js/main.js
@@ -19,95 +19,82 @@ $(function() {
 });
 
 
-var bar = new ProgressBar.SemiCircle(container, {
+// var bar = new ProgressBar.SemiCircle(container, {
+//
+// //determining success
+// function success(){
+//   return document.getElementById('     ').id;
+// }
+// //progress bar
+//
+// //html for progress bar
+//
+// //type="text/css">
+// //<div id="container"></div>
+// function progressBar(){
+// var score = 0;
+// var success = success();
+// var attempts = document.getElementById("addItem").id;
+//
+//
+// var bar = new ProgressBar.SemiCircle(container, {
+//
+//   strokeWidth: 6,
+//   color: '#FFEA82',
+//   trailColor: '#eee',
+//   trailWidth: 1,
+//   easing: 'easeInOut',
+//   duration: 1400,
+//   svgStyle: null,
+//   text: {
+//     value: '',
+//     alignToBottom: false
+//   },
+//   from: {color: '#ff0505'},
+//   to: {color: '##ffffff'},
+//   // Set default step function for all animate calls
+//   step: (state, bar) => {
+//     bar.path.setAttribute('stroke', state.color);
+//     score = 100*(succes/attempts);
+//     if (score === 0) {
+//       bar.setText('');
+//     } else {
+//       bar.setText(score);
+//     }
+//
+//     bar.text.style.color = state.color;
+//   }
+// });
+// bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+// bar.text.style.fontSize = '2rem';
+// percent = succes/attempts;
+// bar.animate(percent);  // Number from 0.0 to 1.0
+// }
 
-//determining success
-function success(){
-  return document.getElementById('     ').id;
-}
-//progress bar
-
-//html for progress bar
-
-//type="text/css">
-//<div id="container"></div>
-function progressBar(){
-var score = 0;
-var success = success();
-var attempts = document.getElementById("addItem").id;
-
-
-var bar = new ProgressBar.SemiCircle(container, {
-
-  strokeWidth: 6,
-  color: '#FFEA82',
-  trailColor: '#eee',
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  svgStyle: null,
-  text: {
-    value: '',
-    alignToBottom: false
-  },
-  from: {color: '#ff0505'},
-  to: {color: '##ffffff'},
-  // Set default step function for all animate calls
-  step: (state, bar) => {
-    bar.path.setAttribute('stroke', state.color);
-    score = 100*(succes/attempts);
-    if (score === 0) {
-      bar.setText('');
-    } else {
-      bar.setText(score);
-    }
-
-    bar.text.style.color = state.color;
-  }
-});
-bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-bar.text.style.fontSize = '2rem';
-percent = succes/attempts;
-bar.animate(percent);  // Number from 0.0 to 1.0
-}
-
-$('#add').click( function() {
+$('#addItem').click( function() {
    var Description = $('#description').val();
    var Day = $('#day').val();
    var Time = $('#time').val();
-  if($("#description").val() == '') {
-    $('#alert').html("<strong>Warning!</strong> You left the description empty");
-    $('#alert').fadeIn().delay(1000).fadeOut();
-    return false;
-   }
-   if($("#day").val() == '') {
-     $('#alert').html("<strong>Warning!</strong> You left the date empty");
-     $('#alert').fadeIn().delay(1000).fadeOut();
-     return false;
-    }
-    if($("#time").val() == '') {
-      $('#alert').html("<strong>Warning!</strong> You left the time empty");
-      $('#alert').fadeIn().delay(1000).fadeOut();
-      return false;
-     }
-     entries[entries.length]={desc:Description, monthday:Day, clock:Time};
-   $('#itemsTable').prepend("<tr><input id='check' name='check' type='checkbox'/><td>" + Description + "</td><td>"+ Day + "</td><td>" + Time"</td><tr>");
+   entries[entries.length]={desc:Description, monthday:Day, clock:Time};
+   $('#itemsTable').prepend("<tr><input id='check' name='check' type='checkbox'/><td>" + Description + "</td><td>"+ Day + "</td><td>" + Time + "</td><tr>");
    $('#form')[0].reset();
    localStorage.setItem('entries', entries);
    return false;
 });
 
-if(localStorage.getItem('entries')) {
-$('#entries').html(localStorage.getItem('entries'));
-}
-});
+// if(localStorage.getItem('entries')) {
+// $('#entries').html(localStorage.getItem('entries'));
+// }
+// });
 function loadin()
 {
-
+var curday = getDate();
+var curmonth = getMonth();
+var hours = getHours();
+var minutes = getMinutes();
 }
 function remove(array, element) {
     const index = array.indexOf(element);
-
     if (index !== -1) {
         array.splice(index, 1);
     }
