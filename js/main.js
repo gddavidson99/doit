5501cb09e7b90b84807a49c9cfc04ca9590ec888
Zach Beder <zachbeder@gmail.com>
2017-10-21 14:03:55 -0400

    PB attempt 1

--- js/main.js
+++ js/main.js
@@ -33,8 +33,8 @@ function success(){
 //<div id="container"></div>
 function progressBar(){
 var score = 0;
-var success = success();
-var attempts = document.getElementById("addItem").id;
+var success = 1//success();
+var attempts = 2//document.getElementById("add").id;
 
 
 var bar = new ProgressBar.SemiCircle(container, {
